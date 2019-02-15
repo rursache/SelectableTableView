578e38942e5c255eb1f413d9fc1d3ec8b736d2bd
Radu Ursache <radu@ursache.ro>
2019-02-15 14:00:03 +0200

    Initial Commit

---
+++ SelectableTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelectableTableView
+//
+//  Created by Radu Ursache on 15/02/2019.
+//  Copyright © 2019 Radu Ursache. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
